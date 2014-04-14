76b3e987e0b8e306b5826291357d2d240f46fd9e
Prelang Builder <builder@prelang.com>
2014-04-14 01:57:57 +0000

    Initial commit; clean Rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Yocifico::Application.config.secret_key_base = '167195ab595b04e7105821d013b05b322b36e4e5dbba3de2ec739c884add59cf4174905f8a925006d06a1e0ef75a2913521748b30c363ee7ddec68dd132f7948'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Yocifico::Application.config.session_store :cookie_store, key: '_yocifico_session'
